45a478513ce77a5aa0fba180af55d18a9aeea704
h-nakajima <2026_04_00247@it-college.co.jp>
2026-08-20 09:25:11 +0900

    chore: resourcesファイルのthymeleaf対応

---
+++ src/main/java/com/example/demo/presentation/controller/pageproperty/TransitionTargetPageNameKeyword.java
@@ -0,0 +1,15 @@
+package com.example.demo.presentation.controller.pageproperty;
+
+public class TransitionTargetPageNameKeyword {
+
+	public static String getArtisanPageList() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	public static String getCustomerPageList() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
---
+++ src/main/java/com/example/demo/presentation/form/LoginUserForm.java
@@ -0,0 +1,15 @@
+package com.example.demo.presentation.form;
+
+public class LoginUserForm {
+
+	public boolean isArtisan() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	public boolean isCustomer() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+}
