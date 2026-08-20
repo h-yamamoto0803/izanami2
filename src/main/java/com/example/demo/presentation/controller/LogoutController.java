d0486185771cd9178024cc9d7d76ac69be4f7888
h-yamamoto <2026_04_00783@it-college.co.jp>
2026-08-20 12:54:44 +0900

    ログアウトの画面遷移部分

---
+++ src/main/java/com/example/demo/presentation/controller/LogoutController.java
@@ -0,0 +1,26 @@
+package com.example.demo.presentation.controller;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.presentation.controller.pageproperty.TransitionTargetPageNameKeyword;
+
+/**
+ * ログアウトに関する処理を担当するControllerです。
+ *
+ * ログアウトボタンが押された際に呼び出され、
+ * 共通メニュー画面へ遷移します。
+ */
+@Controller
+public class LogoutController {
+
+	@PostMapping("/logout")
+	public String logout(HttpSession session) {
+
+	    session.invalidate();
+
+	    return TransitionTargetPageNameKeyword.MENU_HTML;
+	}
+}
