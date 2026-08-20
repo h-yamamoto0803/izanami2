38323db415fee6524d20ef3563b94ee9c03aa0cd
h-nakajima <2026_04_00247@it-college.co.jp>
2026-08-20 12:45:02 +0900

    feat : guest menuへの画面遷移

--- src/main/java/com/example/demo/presentation/controller/pageproperty/TransitionTargetPageNameKeyword.java
+++ src/main/java/com/example/demo/presentation/controller/pageproperty/TransitionTargetPageNameKeyword.java
@@ -1,7 +1,15 @@
 package com.example.demo.presentation.controller.pageproperty;
 
 public class TransitionTargetPageNameKeyword {
-
+	// guest, utill
+		// HTML
+	public static final String MENU_HTML= "menu";
+		// Controller
+	public static final String INDEX_BLANK="";
+	public static final String INDEX_SLASH="/";
+	// artisan
+	
+	// customer
 	public static String getArtisanPageList() {
 		// TODO 自動生成されたメソッド・スタブ
 		return null;
