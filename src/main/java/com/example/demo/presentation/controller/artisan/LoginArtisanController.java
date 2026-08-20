139ae1ffc0bf181b718352b99854f56495804ec7
h-yamamoto <2026_04_00783@it-college.co.jp>
2026-08-20 12:48:58 +0900

    ログインの画面遷移

---
+++ src/main/java/com/example/demo/presentation/controller/artisan/LoginArtisanController.java
@@ -0,0 +1,56 @@
+package com.example.demo.presentation.controller.artisan;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.presentation.controller.pageproperty.TransitionTargetPageNameKeyword;
+import com.example.demo.presentation.form.LoginForm;
+/**
+ * Artisan（職人）のログインに関する画面遷移を担当するControllerです。
+ */
+@Controller
+public class LoginArtisanController {
+
+	/**
+     * Artisanログイン画面を表示します。
+     *
+     * @param loginForm Artisanログイン画面で使用するフォーム
+     * @return Artisanログイン画面
+     */
+	@GetMapping("/artisan/login")
+	public String showLoginArtisan(
+	        @ModelAttribute("loginForm") LoginForm loginForm) {
+
+	  
+	
+
+	
+        /*
+         * Artisanログイン画面を表示します。
+         */
+		  return TransitionTargetPageNameKeyword.ARTISAN_LOGIN_HTML;
+	}
+	/**
+	 * Artisanログイン画面からログイン処理を実行します。。
+	 *
+	 * @param loginForm Artisanログイン画面から送信された入力値
+	 * @return Artisanメニュー画面
+	 */
+	@PostMapping("/artisan/login")
+	public String loginArtisan(
+	        @ModelAttribute("loginForm") LoginForm loginForm,
+	        HttpSession session) {
+
+	    // 職人としてログインしたことをセッションに保存
+	    session.setAttribute("userType", "artisan");
+
+	    /*
+	     * Artisanメニュー画面へ遷移します。
+	     */
+	    return TransitionTargetPageNameKeyword.ARTISAN_MENU_HTML;
+	}
+}
