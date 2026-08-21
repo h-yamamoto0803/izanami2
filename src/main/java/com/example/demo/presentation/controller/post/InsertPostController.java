77854af291488b2aefae8f28c1dc8f06a5cc315c
h-nakajima <2026_04_00247@it-college.co.jp>
2026-08-21 16:31:51 +0900

    feat : 投稿入力フォームのタグ欄を記入形式にして、複数入力可能にした

--- src/main/java/com/example/demo/presentation/controller/post/InsertPostController.java
+++ src/main/java/com/example/demo/presentation/controller/post/InsertPostController.java
@@ -1,8 +1,10 @@
 package com.example.demo.presentation.controller.post;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.domain.service.post.SearchTags;
 import com.example.demo.presentation.controller.pageproperty.TransitionTargetPageNameKeyword;
@@ -19,12 +21,20 @@ public class InsertPostController {
 	 * 投稿作成画面表示
 	 */
 	@GetMapping(TransitionTargetPageNameKeyword.INSERT_POST)
-	public String insertPost(@ModelAttribute InsertPostForm insertPostForm) {
+	public String insertPostGet(@ModelAttribute InsertPostForm insertPostForm) {
 		
 		insertPostForm.setTags(service.getTags());
 		
 		return TransitionTargetPageNameKeyword.POST_CREATE_HTML;
 	}
+	@PostMapping(TransitionTargetPageNameKeyword.INSERT_POST)
+	public String insertPost(@ModelAttribute InsertPostForm insertPostForm,
+			Model model) {
+		model.addAttribute("insertPostForm",insertPostForm);
+		
+		return TransitionTargetPageNameKeyword.POST_CREATE_HTML;
+	}
+	
 	
 	/*
 	 * 投稿作成画面の戻るボタン
