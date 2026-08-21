0be0d1f3da9a4eab06438ca5844416dd443f2e1e
h-nakajima <2026_04_00247@it-college.co.jp>
2026-08-21 13:59:43 +0900

    feat : TransitionTargetPageNameKeyword　ファイルの追加

---
+++ src/main/java/com/example/demo/presentation/controller/pageproperty/TransitionTargetPageNameKeyword.java
@@ -0,0 +1,29 @@
+package com.example.demo.presentation.controller.pageproperty;
+
+public class TransitionTargetPageNameKeyword {
+	// guest, utill
+		// HTML
+	public static final String MENU_HTML= "menu";
+		// Controller
+	public static final String INDEX_BLANK="";
+	public static final String INDEX_SLASH="/";
+	// artisan
+	
+	// customer
+	// Post
+		//Controller
+	public final static String POST_DETAIL = "/post-detail";
+		//HTML
+	public final static String POST_DETAIL_HTML = "post-detail";
+	
+	public static String getArtisanPageList() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	public static String getCustomerPageList() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
