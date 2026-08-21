4fda0036cb1edd240f7dc622f872f0e30e0787a2
k-takeshita <2026_04_00279@it-college.co.jp>
2026-08-21 12:53:38 +0900

    fix: ログインユーザーのキーワードを含むセッションキーワードファイルを追加

---
+++ src/main/java/com/example/demo/presentation/controller/pageproperty/SessionKeyword.java
@@ -0,0 +1,6 @@
+package com.example.demo.presentation.controller.pageproperty;
+
+public class SessionKeyword {
+	// SESSION情報として格納したいログイン者の情報
+	public static final String LOGIN_USER = "loginUser";
+}
