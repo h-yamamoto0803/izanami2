8c435e144b4df0c1dd49be5803efee3de06447d4
h-yamamoto <2026_04_00783@it-college.co.jp>
2026-08-21 11:26:38 +0900

    feat: [ログインformの追加]

--- src/main/java/com/example/demo/presentation/controller/artisan/LoginArtisanController.java
+++ src/main/java/com/example/demo/presentation/controller/artisan/LoginArtisanController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.presentation.controller.pageproperty.TransitionTargetPageNameKeyword;
-import com.example.demo.presentation.form.LoginForm;
+import com.example.demo.presentation.form.LoginUserForm;
 /**
  * Artisan（職人）のログインに関する画面遷移を担当するControllerです。
  */
@@ -18,12 +18,12 @@ public class LoginArtisanController {
     /**
      * Artisanログイン画面を表示します。
      *
-     * @param loginForm Artisanログイン画面で使用するフォーム
+     * @param loginUserForm Artisanログイン画面で使用するフォーム
      * @return Artisanログイン画面
      */
     @GetMapping(TransitionTargetPageNameKeyword.LOGIN_ARTISAN_CONTROLLER)
     public String showLoginArtisan(
-            @ModelAttribute("loginForm") LoginForm loginForm) {
+            @ModelAttribute("loginForm") LoginUserForm loginUserForm) {
 
         /*
          * Artisanログイン画面を表示します。
@@ -34,13 +34,13 @@ public class LoginArtisanController {
     /**
      * Artisanログイン画面からログイン処理を実行します。
      *
-     * @param loginForm Artisanログイン画面から送信された入力値
+     * @param loginUserForm Artisanログイン画面から送信された入力値
      * @param session ログイン情報を保持するセッション
      * @return Artisanメニュー画面
      */
     @PostMapping(TransitionTargetPageNameKeyword.LOGIN_ARTISAN_CONTROLLER)
     public String loginArtisan(
-            @ModelAttribute("loginForm") LoginForm loginForm,
+            @ModelAttribute("loginForm") LoginUserForm loginUserForm,
             HttpSession session) {
 
         // 職人としてログインしたことをセッションに保存
--- src/main/java/com/example/demo/presentation/form/LoginUserForm.java
+++ src/main/java/com/example/demo/presentation/form/LoginUserForm.java
@@ -1,7 +1,12 @@
 package com.example.demo.presentation.form;
 
 public class LoginUserForm {
+	/** メールアドレス */
+    private String email;
 
+    /** パスワード */
+    private String password;
+    
 	public boolean isArtisan() {
 		// TODO 自動生成されたメソッド・スタブ
 		return false;
