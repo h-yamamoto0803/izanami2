d92e4c3dc46bc180084ef0b0562f3d38d22e0555
h-yamamoto <2026_04_00783@it-college.co.jp>
2026-08-25 13:48:00 +0900

    fix: [名前の訂正とメソッドの集約]

---
+++ src/main/java/com/example/demo/presentation/controller/MenuController.java
@@ -0,0 +1,71 @@
+package com.example.demo.presentation.controller;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.domain.service.post.PostService;
+import com.example.demo.presentation.controller.pageproperty.SessionKeyword;
+import com.example.demo.presentation.controller.pageproperty.TransitionTargetPageNameKeyword;
+import com.example.demo.presentation.form.LoginUserForm;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MenuController {
+
+	private final PostService postService;
+
+	@GetMapping(TransitionTargetPageNameKeyword.RETURN_MENU)
+	public String showMenu(
+
+			@RequestParam(required = false) String tag,
+			Model model,
+			HttpSession session) {
+
+		model.addAttribute(
+				"posts",
+				postService.getPostListFromDatabase(tag));
+
+		model.addAttribute(
+				"tags",
+				postService.getAllTags());
+
+		
+		model.addAttribute("selectedTag", tag);
+		
+		LoginUserForm loginUserForm =
+                (LoginUserForm) session.getAttribute(
+                        SessionKeyword.LOGIN_USER
+                );
+
+        // 未ログイン
+        if (loginUserForm == null) {
+            model.addAttribute("userType", "guest");
+
+            return TransitionTargetPageNameKeyword.MENU_HTML;
+        }
+
+        // Artisan
+        if (loginUserForm.isArtisan()) {
+            model.addAttribute("userType", "artisan");
+
+            return TransitionTargetPageNameKeyword.ARTISAN_MENU_HTML;
+        }
+
+        // Customer
+        if (loginUserForm.isCustomer()) {
+            model.addAttribute("userType", "customer");
+
+            return TransitionTargetPageNameKeyword.CUSTOMER_MENU_HTML;
+        }
+
+        // 想定外のユーザータイプ
+        return TransitionTargetPageNameKeyword.MENU_HTML;
+    }
+}
+	
