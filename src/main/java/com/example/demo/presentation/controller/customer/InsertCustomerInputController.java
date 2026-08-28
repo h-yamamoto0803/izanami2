089a7b2308db6ab29596394ccccded5f68b011a8
o-yamamoto <2026_04_00886@it-college.co.jp>
2026-08-28 11:04:59 +0900

    fix[アカウント新規登録に飛べない不具合の修正]

--- src/main/java/com/example/demo/presentation/controller/customer/InsertCustomerInputController.java
+++ src/main/java/com/example/demo/presentation/controller/customer/InsertCustomerInputController.java
@@ -5,8 +5,8 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.domain.service.customer.RegisterCustomer;
 import com.example.demo.presentation.controller.pageproperty.PageReturnAttributeKeyword;
@@ -37,7 +37,7 @@ public class InsertCustomerInputController {
 	}
 	// 登録画面表示
 
-	@PostMapping("/insertCustomerInput")
+	@GetMapping("/insertCustomerInput")
 	public String insertInput(InsertCustomerForm insertCustomerForm, Model model) {
 		model.addAttribute(PageReturnAttributeKeyword.INSERT_CUSTOMER_FORM, insertCustomerForm);
 		return TransitionTargetPageNameKeyword.INSERT_CUSTOMER_INPUT_HTML;
